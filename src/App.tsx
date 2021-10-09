b1b6f2244e8b1125f0cf7986792c8f8369fb9d69
Evgenii Kolesnikov <jekow@yandex.ru>
2021-10-09 18:36:50 +0300

    Add useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import React, { useState } from 'react';
 import './App.css';
 
-export default function App() {
+export const App: React.FC = () => {
+  const [title, setTitle] = useState<string>('');
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const onKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      console.log(title);
+    }
+  };
+
   return (
     <div className='App'>
-      Greenbox
+      <div>Greenbox</div>
+      <input
+        type='text'
+        id='title'
+        value={title}
+        placeholder='add some text'
+        onChange={changeHandler}
+        onKeyPress={onKeyPress}
+      />
     </div>
   );
-}
+};
