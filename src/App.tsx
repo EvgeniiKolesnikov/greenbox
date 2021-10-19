e30fbc4ecd55cf37d138eb50e74c586c3b0f4df2
Evgenii Kolesnikov <jekow@yandex.ru>
2021-10-19 23:32:58 +0300

    Add redux sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 import React, { useState } from 'react';
 import './App.css';
 
+import { useDispatch, useSelector } from 'react-redux';
+import type { RootState, AppDispatch } from './index';
+import { ReactChild, ReactFragment, ReactPortal } from 'react';
+import { addCustomerAction, removeCustomerAction } from './store/customerReducer';
+
+
 export const App: React.FC = () => {
   const [title, setTitle] = useState<string>('');
+  const dispatch = useDispatch<AppDispatch>();
+  const cash = useSelector((state: RootState) => state.cash.cash);
+  const customers = useSelector(
+    (state: RootState) => state.customers.customers
+  );
+  console.log(cash);
+
+  const addCash = (cash: number) => {
+    dispatch({ type: 'ADD_CASH', payload: cash });
+  };
+  const getCash = (cash: number) => {
+    dispatch({ type: 'GET_CASH', payload: cash });
+  };
+  const addCustomer = (name: string) => {
+    const customer = { name, id: Date.now() };
+    dispatch(addCustomerAction(customer));
+  };
+  const removeCustomer = (customer: { name?: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined; id?: any; }) => {
+    dispatch(removeCustomerAction(customer.id));
+  };
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
@@ -25,6 +51,35 @@ export const App: React.FC = () => {
         onChange={changeHandler}
         onKeyPress={onKeyPress}
       />
-    </div>
+    
+    <div className='cash'>cash: {cash}</div>
+
+    <div onClick={() => addCash(Number(prompt()))}>addCash</div>
+    <div onClick={() => getCash(Number(prompt()))}>getCash</div>
+    <div onClick={() => addCustomer(String(prompt()))}>addCustomer</div>
+    {/* <div onClick={() => removeCustomers(String(prompt()))}>delCustomer</div> */}
+    {customers.length > 0 ? (
+      <div>
+        {customers.map(
+          (customer: {
+            name:
+              | boolean
+              | ReactChild
+              | ReactFragment
+              | ReactPortal
+              | null
+              | undefined;
+            id: number
+          }) => (
+            <div onClick={() => removeCustomer(customer)} key={customer.id}>
+              {customer.name}
+            </div>
+          )
+        )}
+      </div>
+    ) : (
+      <div>No customers</div>
+    )}
+  </div>
   );
 };
