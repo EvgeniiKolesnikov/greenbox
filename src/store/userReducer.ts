bd3dedf277bb5784c6109b91c7a0b35a35297126
Evgenii Kolesnikov <jekow@yandex.ru>
2021-10-21 23:37:08 +0300

    Refactoring user

--- src/store/userReducer.ts
+++ src/store/userReducer.ts
@@ -1,44 +1,13 @@
-// const FETCH_USERS = 'FETCH_USERS';
-// const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
-// const FETCH_USERS_ERROR = 'FETCH_USERS_ERROR';
+import { UserAction, UserActionTypes, UserState } from '../types/user';
 
-interface UserState {
-  users: any[];
-  loading: boolean;
-  error: null | string;
-}
-
-enum UserActionTypes {
-  FETCH_USERS = 'FETCH_USERS',
-  FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS',
-  FETCH_USERS_ERROR = 'FETCH_USERS_ERROR',
-}
-
-interface FetchUsersAction {
-  type: UserActionTypes.FETCH_USERS;
-  // type: typeof FETCH_USERS;
-}
-interface FetchUsersSuccessAction {
-  type: UserActionTypes.FETCH_USERS_SUCCESS;
-  payload: any[];
-}
-interface FetchUsersErrorAction {
-  type: UserActionTypes.FETCH_USERS_ERROR;
-  payload: string;
-}
-type UserAction =
-  | FetchUsersAction
-  | FetchUsersSuccessAction
-  | FetchUsersErrorAction;
-
-const defaultState: UserState = {
+const initialState: UserState = {
   users: [],
   loading: false,
   error: null,
 };
 
 export const customerReducer = (
-  state = defaultState,
+  state = initialState,
   action: UserAction
 ): UserState => {
   switch (action.type) {
