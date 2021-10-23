39b2398aaaab36294ab922b140b14c3f1f122d41
Evgenii Kolesnikov <jekow@yandex.ru>
2021-10-24 00:06:33 +0300

    Add pages into TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -7,12 +7,13 @@ export const TodoList: React.FC = () => {
     (state) => state.todo
   );
 
-  const { fetchTodos } = useActions();
+  const { fetchTodos, setTodoPage } = useActions();
+  const pages = [1, 2, 3, 4, 5];
 
   useEffect(() => {
     fetchTodos(page, limit);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [page]);
 
   if (loading) {
     return <h1>Loading...</h1>;
@@ -25,8 +26,23 @@ export const TodoList: React.FC = () => {
   return (
     <div>
       {todos.map((todo) => (
-        <div key={todo.id}>{todo.id} - {todo.title}</div>
+        <div key={todo.id}>
+          {todo.id} - {todo.title}
+        </div>
       ))}
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        {pages.map((p) => (
+          <div
+            style={{
+              border: p === page ? '2px solid green' : '1px solid grey',
+              padding: 10,
+            }}
+            onClick={() => setTodoPage(p)}
+          >
+            {p}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
